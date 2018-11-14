8f3b35605eab236d55f48b063023eb062a6e2890
Chillen <josh.jenkin@live.com>
2018-11-13 21:07:38 -0700

    added some more js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,38 @@
-var targetNumber = "";
+var targetNumber = [];
 var playerNumber = "";
 var counter
 var wins = 0;
 var losses = 0;
 var userGuess = 0;
 
-var targNumb = document.getElementById()
+var computerGuess = targetNumber[Math.floor(Math.random() * 100)];
 
-function startGame() {
+$("#target-number").text(computerGuess);
+$("#wins").text("Wins: " + wins);
+$("#losses").text("Losses: " + losses);
+$("#num-picked").text(userGuess);
 
-}
-$(document).ready(function() {
-    $("#num-picked").text(targetNumber);
-}
+// function startGame() {
+ 
+// }
 
 
-function check() {
-    if ()
-}
+// $("#crys-1").on(click, function() {
+
+
+// }
+
+
+
+
+// function startGame() {
+ 
+// }
+// $(document).ready(function() {
+//     $("#num-picked").text(targetNumber);
+// }
+
+
+// function check() {
+//     if ()
+// }
