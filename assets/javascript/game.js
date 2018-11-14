0e77068474ca8124e7f9948cca30790e5931de6b
Chillen <josh.jenkin@live.com>
2018-11-13 19:08:33 -0700

    coding out, started JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,20 @@
 var targetNumber = "";
+var playerNumber = "";
+var counter
+var wins = 0;
+var losses = 0;
+var userGuess = 0;
 
-$(document).ready(fucntion() {
+var targNumb = document.getElementById()
+
+function startGame() {
+
+}
+$(document).ready(function() {
     $("#num-picked").text(targetNumber);
+}
+
+
+function check() {
+    if ()
 }
