95f0e828be897d583e1d606f849189396f4bf322
Chillen <josh.jenkin@live.com>
2018-11-14 20:46:18 -0700

    finished everything but different pics and random number on pics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,10 +4,7 @@ $(document).ready(function() {
         var counter = 0;
         var wins = 0;
         var losses = 0;
-        var number = [Math.floor(Math.random() * 15) + 1];
-        var number1 = [Math.floor(Math.random() * 15) + 1];
-        var number2 = [Math.floor(Math.random() * 15) + 1];
-        var number3 = [Math.floor(Math.random() * 15) + 1];
+        var number = [0, 0, 0, 0];
 
 
 
@@ -18,33 +15,47 @@ $(document).ready(function() {
 
         function randomNumber() {
             targetNumber = [Math.floor(Math.random() * 50) + 50];
-            console.log(targetNumber)
+            console.log(targetNumber);
             $("#target-number").text(targetNumber);
         }       
-        randomNumber()
-        
+        randomNumber();
+
+        function crystalValues(max, min) {
+            for (var i; i < 3; i++) {
+            number.push[Math.floor(Math.random() * max) + min];
+            }
+            return number;
+        }
+        crystalValues(1,15);
+
         function resetCrystals() {
             for (var i = 0; i < number.length; i++) {
                 var crystals = $("<img>");
+                var crystals2 = $("<img>");
+                var crystals3 = $("<img>");
+                var crystals4 = $("<img>");
                 crystals.addClass("crystals");
                 crystals.attr("src", "./assets/pics/blue-gem.png", "./assets/pics/red-gem.png", "./assets/pics/yel-gem.png", "./assets/pics/green-gem.png");
+                // crystals2.attr("src", "./assets/pics/red-gem.png");
+                // crystals3.attr("src", "./assets/pics/yel-gem.png");
+                // crystals4.attr("src", "./assets/pics/green-gem.png");
                 crystals.attr("value", number[i]);
-                $("#crys").html(crystals);
-                console.log(crystals)
+                $("#crys").append(crystals);
+                $(".crystals").empty();
+                console.log(crystals + crystals2 + crystals3 + crystals4);
             }
         }
-        resetCrystals()
+        resetCrystals();
 
     
         function Reset() {
-            randomNumber ();
+            randomNumber();
+            // crystalValues();
             counter = 0;
             $("#target-number").html(targetNumber);
             $("#wins").html("Wins: " + wins);
             $("#losses").html("Losses: " + losses);
             $("#num-picked").html(counter);
-            $(".crystals").empty();
-            resetCrystals ();
         }
 
         $(".crystals").on("click", function() {
