d61c75b0e7da6be7c9c6eeed695f9ee554c3eb77
Chillen <josh.jenkin@live.com>
2018-11-16 15:25:25 -0700

    got 4 different crystals, added same for loop to crystalImg, number reset still not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,81 +1,77 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    // set variables
+    var targetNumber = "";
+    var counter = 0;
+    var wins = 0;
+    var losses = 0;
+    var number = Array(4).fill(0).map(makeARandomNumber);
+    var crystalImg = ["./assets/pics/blue-gem.png", "./assets/pics/green-gem.png", "./assets/pics/red-gem.png", "./assets/pics/yel-gem.png"]
 
-        var targetNumber = "";
-        var counter = 0;
-        var wins = 0;
-        var losses = 0;
-        var number = [0, 0, 0, 0];
+
+
+    // show wins, loss, countery
+    $("#wins").text("Wins: " + wins);
+    $("#losses").text("Losses: " + losses);
+    $("#num-picked").text(counter);
+
+    // pick random target number
+    function randomNumber() {
+        targetNumber = [Math.floor(Math.random() * 50) + 50];
+        console.log(targetNumber);
+        $("#target-number").text(targetNumber);
+    }
+    randomNumber();
+    // set random number for array
+    function makeARandomNumber() {
+        return Math.floor(Math.random() * 20) + 1;
+    }
+    makeARandomNumber();
+    console.log(number);
+
+    function resetCrystals() {
+        for (var i = 0; i < number.length; i++) {
+            var crystals = $("<img>");
+            crystals.addClass("crystals");
+            crystals.attr("value", number[i]);
+            crystals.attr("src", crystalImg[i]);
+            $(".crystals").empty();
+            $("#crys").append(crystals);
+        }
+    }
+    resetCrystals();
 
 
 
-        
-        $("#wins").text("Wins: " + wins);
-        $("#losses").text("Losses: " + losses);
-        $("#num-picked").text(counter);
-
-        function randomNumber() {
-            targetNumber = [Math.floor(Math.random() * 50) + 50];
-            console.log(targetNumber);
-            $("#target-number").text(targetNumber);
-        }       
+    function reset() {
         randomNumber();
 
-        function crystalValues(max, min) {
-            for (var i; i < 3; i++) {
-            number.push[Math.floor(Math.random() * max) + min];
-            }
-            return number;
+        counter = 0;
+        $("#target-number").html(targetNumber);
+        $("#wins").html("Wins: " + wins);
+        $("#losses").html("Losses: " + losses);
+        $("#num-picked").html(counter);
+    }
+
+    $(".crystals").on("click", function () {
+        var crystalValue = ($(this).attr("value"));
+        crystalValue = parseInt(crystalValue);
+        counter += crystalValue;
+        $("#num-picked").html(counter);
+        if (counter == targetNumber) {
+            wins++;
+            reset();
+            makeARandomNumber();
         }
-        crystalValues(1,15);
+        else if (counter > targetNumber) {
+            losses++;
+            reset();
+            makeARandomNumber();
+        };
 
-        function resetCrystals() {
-            for (var i = 0; i < number.length; i++) {
-                var crystals = $("<img>");
-                var crystals2 = $("<img>");
-                var crystals3 = $("<img>");
-                var crystals4 = $("<img>");
-                crystals.addClass("crystals");
-                crystals.attr("src", "./assets/pics/blue-gem.png", "./assets/pics/red-gem.png", "./assets/pics/yel-gem.png", "./assets/pics/green-gem.png");
-                // crystals2.attr("src", "./assets/pics/red-gem.png");
-                // crystals3.attr("src", "./assets/pics/yel-gem.png");
-                // crystals4.attr("src", "./assets/pics/green-gem.png");
-                crystals.attr("value", number[i]);
-                $("#crys").append(crystals);
-                $(".crystals").empty();
-                console.log(crystals + crystals2 + crystals3 + crystals4);
-            }
-        }
-        resetCrystals();
+        console.log(crystalValue)
+    });
 
-    
-        function Reset() {
-            randomNumber();
-            // crystalValues();
-            counter = 0;
-            $("#target-number").html(targetNumber);
-            $("#wins").html("Wins: " + wins);
-            $("#losses").html("Losses: " + losses);
-            $("#num-picked").html(counter);
-        }
 
-        $(".crystals").on("click", function() {
-            var crystalValue = ($(this).attr("value"));
-            crystalValue = parseInt(crystalValue);
-            counter += crystalValue;       
-            $("#num-picked").html(counter);     
-            if (counter == targetNumber) {
-                wins++;
-                Reset();
-            }
-            else if (counter > targetNumber) {
-                losses++;
-                Reset();
-            };
-            
-            console.log(crystalValue)
-        });
-        
 
-        
 
 })
