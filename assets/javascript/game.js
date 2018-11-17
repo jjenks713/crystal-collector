25056ceb5a225df07da1d112fd6fd40444a68fe6
Chillen <josh.jenkin@live.com>
2018-11-17 14:34:16 -0700

    changed to empty array and function to push numbers to it

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,12 +4,10 @@ $(document).ready(function () {
     var counter = 0;
     var wins = 0;
     var losses = 0;
-    var number = Array(4).fill(0).map(makeARandomNumber);
+    var number = [];
     var crystalImg = ["./assets/pics/blue-gem.png", "./assets/pics/green-gem.png", "./assets/pics/red-gem.png", "./assets/pics/yel-gem.png"]
 
-
-
-    // show wins, loss, countery
+    // show wins, loss, counter
     $("#wins").text("Wins: " + wins);
     $("#losses").text("Losses: " + losses);
     $("#num-picked").text(counter);
@@ -28,37 +26,41 @@ $(document).ready(function () {
         
     }
     makeARandomNumber();
-    console.log(number);
 
-
+    // push random number to array
+    function pushNumberToArray() {
+        number = Array(4).fill(0).map(makeARandomNumber);
+    }
+    
+    // set number value to crystal ID
     function resetCrystals() {
-        for (var i = 0; i < number.length; i++) {
+        $("#crys").empty();
+        pushNumberToArray()
+        for (var i = 0; i < number.length; ++i) {
             var crystals = $("<img>");
             crystals.addClass("crystals");
             crystals.attr("value", number[i]);
             crystals.attr("src", crystalImg[i]);
-            $(".crystals").empty();
             $("#crys").append(crystals);
         }
     }
     resetCrystals();
+    
 
-
-
+    // reset function called after Crystal number exceeds target number
     function reset() {
         randomNumber();
-        number = Array(4).fill(0).map(makeARandomNumber);
-        makeARandomNumber()
+        makeARandomNumber();
         counter = 0;
         $("#target-number").html(targetNumber);
         $("#wins").html("Wins: " + wins);
         $("#losses").html("Losses: " + losses);
         $("#num-picked").html(counter);
-        console.log(number)
     }
-
-
-    $(".crystals").on("click", function () {
+    console.log(number);
+    
+    // on click function linked to crystal ID
+    $("#crys").on("click", "img", function () {
         var crystalValue = ($(this).attr("value"));
         crystalValue = parseInt(crystalValue);
         counter += crystalValue;
@@ -66,14 +68,13 @@ $(document).ready(function () {
         if (counter == targetNumber) {
             wins++;
             reset();
-            
+            resetCrystals();            
         }
         else if (counter > targetNumber) {
             losses++;
             reset();
-            
+            resetCrystals();
         };
-
-        console.log(crystalValue)
+        console.log(crystalValue);
     });
 })
